88f9aa35c2ac2be004fa870e46437feba28d8880
Dani <dani@ubuntu.ubuntu-domain>
2011-04-30 11:05:29 -0400

    moving to bundler

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@
 require 'thread' #added to remove incompatability essues of updating rails
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -55,5 +55,5 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  config.gem "haml"
+
 end
