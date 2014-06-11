9ec008cf6b5143c1600faa290448e9a5401d175e
The Dan <danielgelbart@gmail.com>
2014-06-11 16:27:22 +0300

    Changing seed file to only seed a couple records

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,19 +16,27 @@ Portfolio.seed(:id) do |s|
   s.name = "A"
 end
 
-
-#Stock.seed_many(:ticker,                [{:name => "Berkshire Hathaway", :ticker => "BRK"},                 {:name => "Teva Pharmasutical industries.", :ticker => "TEVA"}])
+# Add a couple stocks, then get data for them
+Stock.seed_many(:ticker,
+                [{:name => "Berkshire Hathaway", :ticker => "BRK.A"},
+                 {:name => "IBM", :ticker => "IBM"},
+                 {:name => "CVX", :ticker => "CVX"},
+                 {:name => "Teva Pharmasutical industries.", :ticker => "TEVA"}])
 
 # Create NYSE stocks from a list maintained in 'nyse_stocks_list.txt'
+=begin
 file = File.open("nyse_stocks_list.txt","r")
-   while (line = file.gets)
-     ticker = line.split.first
-     name = line.split("\t").last.chop
-     s = Stock.create(:ticker => ticker, :name => name)
-     if !s.id.nil?
-       puts "created #{s.ticker}"
-       s.scrape_data
-     end
 
-   end
+
+while (line = file.gets)
+  ticker = line.split.first
+  name = line.split("\t").last.chop
+  s = Stock.create(:ticker => ticker, :name => name)
+  if !s.id.nil?
+    puts "created #{s.ticker}"
+    s.scrape_data
+  end
+end
+
 file.close
+=end #of seedin all stocks
