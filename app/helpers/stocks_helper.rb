07159cc7774d0a5f422905f68b7d81081f3c4369
The Dan <danielgelbart@gmail.com>
2015-03-08 16:33:37 +0200

    Added view editing for compnay type

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,11 +1,11 @@
 module StocksHelper
 
   def add_data_link(form_builder)
-    link_to_function 'Add data for another year' do |page|
+    link_to_function 'Add data for another year', nil, do |page|
       form_builder.fields_for :balance_sheet, BalanceSheet.new, :child_index => 'NEW_RECORD' do |f|
         html = render(:partial => 'balance_sheet', :locals => { :ad_form => f })
         page << "$('finantials').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
-          
+
       end
     end
   end
@@ -24,11 +24,11 @@ module StocksHelper
   end
 
   def add_earnings_link(form_builder)
-    link_to_function 'Add Earnings data for another year' do |page|
+    link_to_function 'Add Earnings data for another year', nil, do |page|
       form_builder.fields_for :ep, Ep.new, :child_index => 'NEW_RECORD' do |f|
         html = render(:partial => 'ep', :locals => { :e_form => f })
         page << "$('eps').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
-          
+
       end
     end
   end
